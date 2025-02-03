5213b1d28c564502d058702c610c026426e4b1dc
Ron-Chandrayan <chandrayanpaul27@gmail.com>
2025-02-03 13:42:06 +0530

    third commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose=require("mongoose");
+const C_Division=require("./models/info.js");
+app.set('view engine', 'ejs');
+app.use('/static',express.static('static'));
+app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+async function hey(){
+    await mongoose.connect("mongodb+srv://chandrayanpaul27:chandrayan%402006@cluster0.7ik3i.mongodb.net/sies");
+  }
+  hey();
+
+app.get('/', async (req, res) => {
+    try {
+        const datas = await C_Division.find().sort({rollNo:1}); // Fetch all documents from c_division
+        if (datas.length === 0) {
+            console.log('No data found');
+        } else {
+            console.log('Fetched Data:', datas[0].name);
+            console.log(datas.length);
+            datas[7].sgpi=0 ;
+            return res.render('index', { datas ,sgpi: datas[7].sgpi});
+        }
+    } catch (err) {
+        console.log("Error fetching data:", err);
+    }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ models/info.js
@@ -0,0 +1,66 @@
+const mongoose=require("mongoose");
+const cDivisionSchema = new mongoose.Schema({
+    rollNo: String,
+    name: String,
+    subjects: {
+        applied_mathematics: {
+            ese: Number,
+            ise: Number,
+            mse: Number,
+            total: Number
+        },
+        applied_physics: {
+            ese: Number,
+            ise: Number,
+            mse: Number,
+            total: Number
+        },
+        beee: {
+            ese: Number,
+            ise: Number,
+            mse: Number,
+            total: Number
+        },
+        c_programming: {
+            ese: Number,
+            ise: Number,
+            mse: Number,
+            total: Number
+        },
+        amrd: {
+            ese: Number,
+            ise: Number,
+            mse: Number,
+            total: Number
+        },
+        lab: {
+            applied_physics_lab: Number,
+            beee_lab: Number,
+            cp_lab: Number,
+            amrd_lab: Number
+        }
+    },
+    additional_courses: {
+        workshop: {
+            grade: String,
+            grade_points: Number,
+            credits: Number
+        },
+        health_wellness: {
+            grade: String,
+            grade_points: Number,
+            credits: Number
+        },
+        universal_human_values: {
+            grade: String,
+            grade_points: Number,
+            credits: Number
+        }
+    },
+    total_marks: Number,
+    result: String,
+    sgpi: Number
+});
+
+const C_Division = mongoose.model('C_Division', cDivisionSchema);
+module.exports=C_Division;
